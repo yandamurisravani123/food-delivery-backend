da2bab6add874ae346185918a29359e902d1a441
chandrakanthd220 <chandrakanth.d220@gmail.com>
2026-06-05 09:38:11 +0530

    added

--- app/models/restaurant.py
+++ app/models/restaurant.py
@@ -39,11 +39,11 @@ class Restaurant(Base):
         nullable=False,
         index=True
     )
+logo_url = Column(
+    String(255),
+    nullable=True
+)
 
-    logo_url = Column(
-        String(255),
-        nullable=True
-    )
 
     cuisine_types = Column(
         String(255),
